53ab027ecade2fe130ffe9c9c5e4b6861b5d20ee
Tamila Yudina <79252896+Taaamilaaa@users.noreply.github.com>
2025-06-18 14:48:24 +0200

    mowes DOM elements to a separate file

---
+++ js/elements.js
@@ -0,0 +1,5 @@
+export const generateArrButton = document.getElementById('generate');
+export const cleanArrButton = document.getElementById('clean');
+export const container = document.getElementById('container');
+export const drowButton = document.getElementById('drow');
+export const bubbleSortButton = document.getElementById('bubbleSort');
