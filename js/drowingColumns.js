c6c8cddb97ae1110f4363909a6ec4fafbfb69ae2
Tamila Yudina <79252896+Taaamilaaa@users.noreply.github.com>
2025-07-04 14:23:42 +0200

    Adds transparency to the fields depending on the value.

--- js/drowingColumns.js
+++ js/drowingColumns.js
@@ -4,19 +4,43 @@ import { drowButton, container } from './elements.js';
 //рисуем массив
 
 export function columnGenerator(a) {
+  const transparency = colorColumns(a);
   const height = a * 2;
   const column = document.createElement('div');
   column.style.width = '10px';
   column.style.height = `${height}px`;
-  column.style.backgroundColor = 'teal';
+  column.style.backgroundColor = `rgba(156, 26, 103  , ${transparency})`;
   column.style.marginLeft = '5px';
   column.style.marginRight = '5px';
+  column.style.border = 'solid, 1px, grey';
   column.title = a;
   column.id = a;
   container.append(column);
   drowButton.disabled = true;
 }
+function colorColumns(a) {
+  const min = 2;
+  const max = 150;
+  const minAlpha = 0.2;
+  const maxAlpha = 1;
 
+  let alpha = (a - min) / (max - min);
+  console.log(alpha);
+  alpha = minAlpha + alpha * (maxAlpha - minAlpha);
+  return alpha.toFixed(2);
+
+  // let alpha;
+  // if (a < 10 || a === 10) {
+  //   alpha = `0.00${a}`;
+  // } else if (a > 10 && a < 100) {
+  //   alpha = `0.0${a}`;
+  // } else if (a > 100 || a === 100) {
+  //   alpha = `0.${a}`;
+  // }
+  // const transparency = Number(alpha) + 0.2;
+  // const result = transparency.toFixed(2);
+  // return result;
+}
 export function arrToCol(newArr) {
   newArr.map(el => columnGenerator(el));
   return;
