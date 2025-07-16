70aae494c15f130356d1e37eca5391bdc8291dea
Tamila Yudina <79252896+Taaamilaaa@users.noreply.github.com>
2025-07-16 15:48:34 +0200

    Update cancelFlag.js

--- js/sorting/cancelFlag.js
+++ js/sorting/cancelFlag.js
@@ -2,10 +2,8 @@ export let cancelRequested = false;
 
 export function requestCancel() {
   cancelRequested = true;
-  console.log(cancelRequested);
 }
 
 export function resetCancelFlag() {
   cancelRequested = false;
-  console.log(cancelRequested);
 }
